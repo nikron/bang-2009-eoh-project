0bd9951575daae3703c1aca75bbcb5841759354d
Nikhil Samith Bysani <nbysani2@illinois.edu>
2008-12-20 01:10:28 -0600

    Mainpage documenation on how the project works.

--- src/app/main.c
+++ src/app/main.c
@@ -1,3 +1,32 @@
+/**
+ * \file main.c
+ * \author Nikhil Bysani
+ * \date Decemeber 20, 2009
+ *
+ * \brief The main function that sets up everything and contains most of the explanatory documentation.
+ */
+
+/**
+ * \mainpage BANG Engineering Open House Project
+ *
+ * \section intro_sec Introduction
+ *
+ * This project is to build an application that can distribute an arbitrary  job/task/load
+ * to other computers on the network.
+ *
+ * The structure of the program is supposed to be divided up into layers similarly to the
+ * Model View Controller (MVC) model.  However, it wont really follow MVC that much.  Most
+ * things are done through the project library, known as the BANG library.
+ *
+ * \section lib_sec BANG Library
+ *
+ * The library operates in the following manner:
+ *
+ * An application initializes it, and then installs signal handlers.  Those handlers will be able to
+ * perform the vast majority of the actions that application would want to take based on the library.
+ * However, there are a few exceptions, such as loading a module which critical for this project
+ * to actually do something useful (loading a module will probably generate some signals!).
+ */
 #include"../base/core.h"
 int main(int argc, char **argv) {
 	BANG_init(&argc,argv);

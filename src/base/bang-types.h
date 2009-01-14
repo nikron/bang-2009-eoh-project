f6bbe480d3eb88d068cd476bc49bd6663c10bf3b
Nikhil Samith Bysani <nbysani2@illinois.edu>
2009-01-14 18:10:19 -0500

    Some structs so we can manipulate each module.

--- src/base/bang-types.h
+++ src/base/bang-types.h
@@ -50,6 +50,21 @@ typedef struct {
 	char *path;
 } BANG_module;
 
+typedef struct {
+	int *peer_ids;
+	unsigned int num_peers; 
+} BANG_module_peerset;
+
+typedef struct {
+	BANG_module *module;
+	BANG_module_peerset *peers;
+} BANG_module_register;
+
+typedef struct {
+	BANG_module_register *registery;
+	unsigned int length;
+} BANG_module_registery;
+
 /**
  * This is the structure that is sent to each signal handler.
  */

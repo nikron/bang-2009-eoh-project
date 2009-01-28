6db3502eb697147d13ef5212784afbc3759d7763
nbysani2 <nbysani2@69d76c3e-0761-0410-948c-9895a8bb34fc>
2009-01-28 05:59:12 +0000

    Added the signal handling method to the server menu file.
    git-svn-id: https://subversion.cs.uiuc.edu/svn/bang/eoh2009@74 69d76c3e-0761-0410-948c-9895a8bb34fc

--- src/app/server-menu.c
+++ src/app/server-menu.c
@@ -1,38 +1,71 @@
-#include<gtk/gtk.h>
 #include"../base/bang.h"
+#include<gtk/gtk.h>
 
-static GtkWidget *server = NULL;
-static GtkWidget *servermenu = NULL;
 /**
  * Start Stop Server
  */
 static GtkWidget *ssserver = NULL;
-static GtkWidget *server_pref = NULL;
 
-static void change_server_status() {
+static void server_status(int signal, int num_args, void **args) {
+
+	gdk_threads_enter();
+	if (ssserver == NULL) {
+		gdk_threads_leave();
+		return;
+	}
+
+	switch (signal) {
+		case BANG_LISTEN_FAIL:
+			break;
+		case BANG_BIND_FAIL:
+			break;
+		case BANG_BIND_SUC:
+			break;
+		case BANG_GADDRINFO_FAIL:
+			break;
+		case BANG_BIND_SUC:
+			break;
+		case BANG_SERVER_STARTED:
+			break;
+	}
+
+	gdk_threads_leave();
+
+
+	int i = 0;
+	for (i = 0; i < num_args; ++i) {
+		free(args[i]);
+	}
+
+	free(args);
+}
+
+static void change_server_status(GtkWidget widget) {
 	if(BANG_is_server_running()) {
 		BANG_server_stop();
-		/* gtk_widget_set_sensitive(widget,FALSE); */
+		gtk_widget_set_sensitive(widget,FALSE);
 	} else {
 		BANG_server_start(NULL);
-		/* gtk_widget_set_sensitive(widget,FALSE); */
+		gtk_widget_set_sensitive(widget,FALSE);
 	}
 }
 
-
 GtkWidget* BMACHINE_setup_server_menu() {
-	server = gtk_menu_item_new_with_label("Server");
-	servermenu = gtk_menu_new();
+	GtkWidget *server = gtk_menu_item_new_with_label("Server");
+	GtkWidget *servermenu = gtk_menu_new();
 
 	ssserver = gtk_image_menu_item_new_from_stock(GTK_STOCK_CONNECT,NULL);
 	gtk_label_set_text(GTK_LABEL(gtk_bin_get_child(GTK_BIN(ssserver))),"Start Server");
 	g_signal_connect(G_OBJECT(ssserver), "activate", G_CALLBACK(change_server_status), NULL);
 
-	server_pref = gtk_image_menu_item_new_from_stock(GTK_STOCK_PREFERENCES,NULL);
-
 	gtk_menu_shell_append(GTK_MENU_SHELL(servermenu),ssserver);
-	gtk_menu_shell_append(GTK_MENU_SHELL(servermenu),server_pref);
 	gtk_menu_item_set_submenu(GTK_MENU_ITEM(server),servermenu);
+	BANG_install_sighandler(BANG_LISTEN_FAIL,&change_server_status);
+	BANG_install_sighandler(BANG_BIND_FAIL,&change_server_status);
+	BANG_install_sighandler(BANG_BIND_SUC,&change_server_status);
+	BANG_install_sighandler(BANG_GADDRINFO_FAIL,&change_server_status);
+	BANG_install_sighandler(BANG_BIND_SUC,&change_server_status);
+	BANG_install_sighandler(BANG_SERVER_STARTED,&change_server_status);
 
 	return server;
 }

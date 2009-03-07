4c730a9f09597b8a95368400775f3fe76e9d6f8b
nbysani2 <nbysani2@69d76c3e-0761-0410-948c-9895a8bb34fc>
2009-03-07 21:59:32 +0000

    more routing, and it compiles
    git-svn-id: https://subversion.cs.uiuc.edu/svn/bang/eoh2009@122 69d76c3e-0761-0410-948c-9895a8bb34fc

--- src/base/bang-routing.c
+++ src/base/bang-routing.c
@@ -19,7 +19,7 @@ enum remote_route {
 typedef struct {
 	int peer_id;
 	char *module_name;
-	char *module_version;
+	unsigned char *module_version;
 } peer_route;
 
 typedef struct {
@@ -28,10 +28,26 @@ typedef struct {
 	BANG_module *mr;
 } peer_or_module;
 
+static peer_or_module *new_pom_peer_route(int peer_id, char *module_name, unsigned char *module_version);
 static BANG_request* create_request(enum BANG_request_types request, uuid_t authority, uuid_t peer);
+static BANG_request* create_request_with_message(enum BANG_request_types request, char *message);
 static BANG_request* create_request_with_job(enum BANG_request_types request, uuid_t authority, uuid_t peer, BANG_job *job);
 
 static BANG_hashmap *routes = NULL;
+
+static peer_or_module *new_pom_peer_route(int peer_id, char *module_name, unsigned char *module_version) {
+	peer_or_module *new = malloc(sizeof(peer_or_module));
+
+	new->remote = REMOTE;
+
+	new->pr = malloc(sizeof(peer_route));
+
+	new->pr->peer_id = peer_id;
+	new->pr->module_name = module_name;
+	new->pr->module_version = module_version;
+
+	return new;
+}
 
 static BANG_request* create_request(enum BANG_request_types request, uuid_t authority, uuid_t peer) {
 	int request_length = 40;
@@ -41,6 +57,10 @@ static BANG_request* create_request(enum BANG_request_types request, uuid_t auth
 	memcpy(request_data + 16,peer,16);
 
 	return new_BANG_request(request,request_data,request_length);
+}
+
+static BANG_request* create_request_with_message(enum BANG_request_types request, char *message) {
+	return new_BANG_request(request,message,strlen(message));
 }
 
 static BANG_request* create_request_with_job(enum BANG_request_types request, uuid_t authority, uuid_t peer, BANG_job *job) {
@@ -63,6 +83,7 @@ void BANG_route_job(uuid_t authority, uuid_t peer, BANG_job *job) {
 	assert(!uuid_is_null(peer));
 
 	peer_or_module *route = BANG_hashmap_get(routes,&peer);
+	if (route == NULL) return;
 
 	if (route->remote == LOCAL) {
 		BANG_module_callback_job(route->mr,job,authority,peer);
@@ -70,7 +91,7 @@ void BANG_route_job(uuid_t authority, uuid_t peer, BANG_job *job) {
 	} else {
 
 		BANG_request *request = create_request_with_job(BANG_SEND_JOB_REQUEST,authority,peer,job);
-		BANG_request_peer(pr->peer_id,request);
+		BANG_request_peer_id(route->pr->peer_id,request);
 	}
 }
 
@@ -92,14 +113,15 @@ void BANG_route_finished_job(uuid_t authority, uuid_t peer, BANG_job *job) {
 	assert(!uuid_is_null(peer));
 
 	peer_or_module *route = BANG_hashmap_get(routes,&authority);
+	if (route == NULL) return;
 
 	if (route->remote == LOCAL) {
 		BANG_module_callback_job_finished(route->mr,job,authority,peer);
 
 	} else {
 
-		BANG_request *request = create_request_with_job(BANG_SEND_FINISHED_JOB_RQUEST,authority,peer,job)
-		BANG_request_peer(pr->peer_id,request);
+		BANG_request *request = create_request_with_job(BANG_SEND_FINISHED_JOB_REQUEST,authority,peer,job);
+		BANG_request_peer_id(route->pr->peer_id,request);
 	}
 }
 
@@ -109,14 +131,15 @@ void BANG_route_request_job(uuid_t peer, uuid_t authority) {
 	assert(!uuid_is_null(peer));
 
 	peer_or_module *route = BANG_hashmap_get(routes,&authority);
+	if (route == NULL) return;
 
 	if (route->remote == LOCAL) {
-		BANG_module_callback_job_requst(route->mr,authority,peer);
+		BANG_module_callback_job_request(route->mr,authority,peer);
 
 	} else {
 
-		BANG_request *request = create_request(BANG_SEND_FINISHED_JOB_RQUEST,authority,peer);
-		BANG_request_peer(pr->peer_id,request);
+		BANG_request *request = create_request(BANG_SEND_REQUEST_JOB_REQUEST,authority,peer);
+		BANG_request_peer_id(route->pr->peer_id,request);
 	}
 }
 
@@ -126,7 +149,16 @@ void BANG_route_assertion_of_authority(uuid_t authority, uuid_t peer) {
 	assert(!uuid_is_null(peer));
 
 	peer_or_module *route = BANG_hashmap_get(routes,&peer);
-	route = NULL;
+	if (route == NULL) return;
+
+	if (route->remote == LOCAL) {
+		BANG_module_callback_job_available(route->mr,authority,peer);
+
+	} else {
+
+		BANG_request *request = create_request(BANG_SEND_AVAILABLE_JOB_REQUEST,authority,peer);
+		BANG_request_peer_id(route->pr->peer_id,request);
+	}
 }
 
 void BANG_route_send_message(uuid_t peer, char *message) {
@@ -135,7 +167,16 @@ void BANG_route_send_message(uuid_t peer, char *message) {
 	assert(!uuid_is_null(peer));
 
 	peer_or_module *route = BANG_hashmap_get(routes,&peer);
-	route = NULL;
+	if (route == NULL) return;
+
+	if (route->remote == LOCAL) {
+		fprintf(stderr,"%s",message);
+
+	} else {
+
+		BANG_request *request = create_request_with_message(BANG_DEBUG_REQUEST,message);
+		BANG_request_peer_id(route->pr->peer_id,request);
+	}
 }
 
 int BANG_route_get_peer_id(uuid_t peer) {
@@ -172,7 +213,9 @@ void BANG_register_module_route(BANG_module *module) {
 	assert(module != NULL);
 }
 
-void BANG_register_peer_route(uuid_t uuid, int peer, char *module_name, unsigned char* module_version) {
+void BANG_register_peer_route(uuid_t uuid, int peer_id, char *module_name, unsigned char* module_version) {
+	peer_or_module *pom = new_pom_peer_route(peer_id,module_name,module_version);
+	BANG_hashmap_set(routes,&uuid,pom);
 }
 
 void BANG_deregister_route(uuid_t route) {
